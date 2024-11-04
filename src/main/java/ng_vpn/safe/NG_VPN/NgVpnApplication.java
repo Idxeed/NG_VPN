26df340a63b93f0b3198c69881311920fdf4b847
Idxeed <117141740+Idxeed@users.noreply.github.com>
2024-11-04 22:03:33 +0300

    Add files via upload

---
+++ src/main/java/ng_vpn/safe/NG_VPN/NgVpnApplication.java
@@ -0,0 +1,13 @@
+package ng_vpn.safe.NG_VPN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgVpnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgVpnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ng_vpn/safe/NG_VPN/NgVpnApplicationTests.java
@@ -0,0 +1,13 @@
+package ng_vpn.safe.NG_VPN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgVpnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
